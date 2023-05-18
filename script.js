fe8ad99966a89a75b2e791e434284fadde75b154
unknown <chrisactive06@gmail.com>
2023-05-18 20:21:50 +0100

    Added Javascript that adds the todo

--- script.js
+++ script.js
@@ -74,3 +74,99 @@ mode.addEventListener("click", (e) => {
         getMode(id)
     }
 })
+
+
+
+
+
+// handling inputs
+let createTodo = document.getElementById("to-do-input");
+let toDos = document.querySelector(".to-dos");
+let clearFilter = document.querySelector(".clear-filter")
+
+// reset input field when window loads
+window.addEventListener("load", () => {
+    createTodo.value = ""
+})
+
+// add todo when the user clicks the enter button
+createTodo.addEventListener("focus", () => {
+    createTodo.addEventListener("keypress", (event) => {
+        if(event.key === "Enter"){
+            ifEmpty()
+            toDoCompleted()
+        }
+    })
+})
+
+// function that checks if input field is empty
+function ifEmpty(){
+    if(createTodo.value == ""){
+        return;
+    }
+    else{
+        ifFilled();
+    }
+}
+
+// function that chcecks if input field is filled
+function ifFilled(){
+    let box = document.createElement("p")
+    box.innerHTML = 
+    `<div class="add-to-do to-do-box to-do-cont">
+        <div class = "circle to-do-circle"></div>
+        <p class="to-do-text">${createTodo.value}</p>
+        <img class = "delete" src="./images/icon-cross.svg">
+    </div>`
+    toDos.appendChild(box)
+    clearFilter.style.display = "grid"
+    createTodo.value = ""
+}
+
+
+// Function that underlines the todo when the user checks it
+function getCompleteStatus(circle, text, id){
+    let completeStatus = [
+        {
+            background: "linear-gradient(to bottom, hsl(192, 100%, 67%), hsl(280, 87%, 65%))",
+            border: "none",
+            innerHTML: `<img class="mode" id="0" src="./images/icon-check.svg" alt="">`,
+            textDecoration: "line-through",
+            textColor: "hsl(233, 11%, 84%)"
+        },
+        {
+            background: "none",
+            border: "1px solid hsl(233, 11%, 84%)",
+            innerHTML: "",
+            textDecoration: "none",
+            textColor: "hsl(236, 9%, 61%)"
+        }
+    ]
+    circle.style.background = completeStatus[id].background
+    circle.style.border = completeStatus[id].border
+    circle.innerHTML = completeStatus[id].innerHTML
+    text.style.textDecoration = completeStatus[id].textDecoration
+    text.style.color = completeStatus[id].textColor
+}
+
+
+function toDoCompleted(){
+    let toDoCircles = document.querySelectorAll(".to-do-circle")
+    let toDoText = document.querySelectorAll(".to-do-text")
+    for(let i = 0; i < toDoCircles.length; i++)
+    {
+        toDoCircles[i].addEventListener("click", () => {
+            if(toDoCircles[i].classList.contains("to-do-circle")){
+                let id = 0;
+                getCompleteStatus(toDoCircles[i], toDoText[i], id)
+                toDoCircles[i].classList.remove("to-do-circle")
+            }else{
+                let id = 1;
+                getCompleteStatus(toDoCircles[i], toDoText[i], id)
+                toDoCircles[i].classList.add("to-do-circle")
+            }
+        })
+    }
+    console.log(toDoCircles.length)
+    
+}
