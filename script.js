9d1cee5b95acc48991d171f8a68787bab04ea164
unknown <chrisactive06@gmail.com>
2023-09-10 14:55:21 +0100

    Updated my Todo app

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
-let mode = document.querySelector(".mode");
+// Theme section
+let themeIcon = document.querySelector(".mode");
 let body = document.getElementById("body");
 let head = document.querySelector(".head");
-let toDoContainer = document.querySelectorAll(".to-do-cont")
-let toDoBox = document.querySelectorAll(".to-do-box")
-let circles = document.querySelectorAll(".circle")
-let toDoText = document.querySelectorAll(".to-do-input")
+let currentTheme = "white";
+let width = window.innerWidth
 
 
+// Data array for modes
+let mode = [
+    {
+        bgMobile: "url(./images/bg-mobile-dark.jpg)",
+        bgDesktop: "url(./images/bg-desktop-dark.jpg)",
+        body: "hsl(235, 21%, 11%)",
+        toDoContainer: "hsl(235, 24%, 19%)",
+        border: "hsl(237, 14%, 26%)",
+        toDoText: "hsl(234, 39%, 85%",
+    },
+    {
+        bgMobile: "url(./images/bg-mobile-light.jpg)",
+        bgDesktop: "url(./images/bg-desktop-light.jpg)",
+        body: "hsl(236, 33%, 92%)",
+        toDoContainer: "hsl(0, 0%, 98%)",
+        border: "hsl(233, 11%, 84%)",
+        toDoText: "hsl(235, 19%, 35%)",
+    }
+]
 
-// handle theme changes
-function setMode(mode, id, width){
+// Set the Mode Background colour and image
+let setBgs = (mode, id, width) => {
     if(width < 720)
     {
         head.style.backgroundImage = mode[id].bgMobile
     }else{
         head.style.backgroundImage = mode[id].bgDesktop
     }
-    body.style.backgroundColor = mode[id].body
+    body.style.backgroundColor = mode[id].body;
+}
+
+// Set the Mode Todo colour
+let setTodos = (mode, id) => {
+    let toDoContainer = document.querySelectorAll(".to-do-cont")
+    let toDoBox = document.querySelectorAll(".to-do-box")
+    let circles = document.querySelectorAll(".circle")
+    let toDoText = document.querySelectorAll(".to-do-input");
+
     toDoContainer.forEach((cont) => {
         cont.style.backgroundColor = mode[id].toDoContainer
     })
@@ -31,142 +58,68 @@ function setMode(mode, id, width){
     })
 }
 
-// object that set values of mode
-function getMode(id){
-    let mode = [
-        {
-            bgMobile: "url(./images/bg-mobile-dark.jpg)",
-            bgDesktop: "url(./images/bg-desktop-dark.jpg)",
-            body: "hsl(235, 21%, 11%)",
-            toDoContainer: "hsl(235, 24%, 19%)",
-            border: "hsl(237, 14%, 26%)",
-            toDoText: "hsl(234, 39%, 85%",
-        },
-        {
-            bgMobile: "url(./images/bg-mobile-light.jpg)",
-            bgDesktop: "url(./images/bg-desktop-light.jpg)",
-            body: "hsl(236, 33%, 92%)",
-            toDoContainer: "hsl(0, 0%, 98%)",
-            border: "hsl(233, 11%, 84%)",
-            toDoText: "hsl(235, 19%, 35%)",
-        }
-    ]
-    let width = window.innerWidth
-
-    setMode(mode, id, width)
-}
-
-
-
-// Event listener to change themes
-mode.addEventListener("click", (e) => {
+// Event Listener to change themes
+themeIcon.addEventListener("click", (e) => {
     let id = e.target.id;
-    if(id == "0")
-    {
-        mode.removeAttribute("src");
-        mode.setAttribute("src", "./images/icon-sun.svg");
-        getMode(id)
-        mode.id = "1"
+    if(id == "0"){
+        themeIcon.setAttribute("src", "./images/icon-sun.svg");
+        setBgs(mode, id, width);
+        setTodos(mode, id);
+        themeIcon.id = "1"
+        currentTheme = "black";
     }else{
-        mode.removeAttribute("src");
-        mode.setAttribute("src", "./images/icon-moon.svg");
-        mode.id = "0"
-        getMode(id)
+        themeIcon.setAttribute("src", "./images/icon-moon.svg");
+        themeIcon.id = "0"
+        setBgs(mode, id, width);
+        setTodos(mode, id);
+        currentTheme = "white"
     }
 })
 
 
-
-
-
-// handling inputs
-let createTodo = document.getElementById("to-do-input");
-let toDos = document.querySelector(".to-dos");
+// Form section
+let form = document.querySelector("form");
+let toDos = document.querySelector("ul");
 let clearFilter = document.querySelector(".clear-filter")
 
-// reset input field when window loads
-window.addEventListener("load", () => {
-    createTodo.value = ""
-})
-
-// add todo when the user clicks the enter button
-createTodo.addEventListener("focus", () => {
-    createTodo.addEventListener("keypress", (event) => {
-        if(event.key === "Enter"){
-            ifEmpty()
-            toDoCompleted()
-        }
-    })
-})
-
-// function that checks if input field is empty
-function ifEmpty(){
-    if(createTodo.value == ""){
-        return;
-    }
-    else{
-        ifFilled();
-    }
-}
-
-// function that chcecks if input field is filled
-function ifFilled(){
-    let box = document.createElement("p")
-    box.innerHTML = 
-    `<div class="add-to-do to-do-box to-do-cont">
-        <div class = "circle to-do-circle"></div>
-        <p class="to-do-text">${createTodo.value}</p>
+// Template to create a new todo
+let createTemplate = (todo) => {
+    let html = `<li class="add-to-do to-do-box to-do-cont">
+        <div class = "circle to-do-circle"><img src="./images/icon-check.svg"></div>
+        <p class="to-do-text">${todo}</p>
         <img class = "delete" src="./images/icon-cross.svg">
-    </div>`
-    toDos.appendChild(box)
-    clearFilter.style.display = "grid"
-    createTodo.value = ""
+    </li>`
+    toDos.innerHTML += html;
 }
 
-
-// Function that underlines the todo when the user checks it
-function getCompleteStatus(circle, text, id){
-    let completeStatus = [
-        {
-            background: "linear-gradient(to bottom, hsl(192, 100%, 67%), hsl(280, 87%, 65%))",
-            border: "none",
-            innerHTML: `<img class="mode" id="0" src="./images/icon-check.svg" alt="">`,
-            textDecoration: "line-through",
-            textColor: "hsl(233, 11%, 84%)"
-        },
-        {
-            background: "none",
-            border: "1px solid hsl(233, 11%, 84%)",
-            innerHTML: "",
-            textDecoration: "none",
-            textColor: "hsl(236, 9%, 61%)"
-        }
-    ]
-    circle.style.background = completeStatus[id].background
-    circle.style.border = completeStatus[id].border
-    circle.innerHTML = completeStatus[id].innerHTML
-    text.style.textDecoration = completeStatus[id].textDecoration
-    text.style.color = completeStatus[id].textColor
-}
-
-
-function toDoCompleted(){
-    let toDoCircles = document.querySelectorAll(".to-do-circle")
-    let toDoText = document.querySelectorAll(".to-do-text")
-    for(let i = 0; i < toDoCircles.length; i++)
-    {
-        toDoCircles[i].addEventListener("click", () => {
-            if(toDoCircles[i].classList.contains("to-do-circle")){
-                let id = 0;
-                getCompleteStatus(toDoCircles[i], toDoText[i], id)
-                toDoCircles[i].classList.remove("to-do-circle")
-            }else{
-                let id = 1;
-                getCompleteStatus(toDoCircles[i], toDoText[i], id)
-                toDoCircles[i].classList.add("to-do-circle")
-            }
-        })
+// handling the form
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    let todo = form.toDoInput.value.trim();
+    if (todo.length){
+        createTemplate(todo);
+        form.reset();
     }
-    console.log(toDoCircles.length)
-    
-}
+    clearFilter.style.display = "grid";
+    if(currentTheme == "black"){
+        setTodos(mode, 0);
+    }else if(currentTheme == "white"){
+        setTodos(mode, 1);
+    }
+})
+
+// delete a todo
+toDos.addEventListener("click", e => {
+    if (e.target.className == "delete"){
+        e.target.parentElement.remove();
+    }
+})
+
+// Handles marking of todo as completed
+toDos.addEventListener("click", e => {
+    if (e.target.classList.contains("to-do-circle")){
+        e.target.parentElement.classList.toggle("to-do-checked")
+    }else if(e.target.parentElement.classList.contains("to-do-circle")){
+        e.target.parentElement.parentElement.classList.toggle("to-do-checked")
+    }
+})
