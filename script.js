92c139324f7fdbf373c3f453f31f3499d8a6c0df
unknown <chrisactive06@gmail.com>
2023-09-11 11:41:48 +0100

    Added more functionality like filter

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let themeIcon = document.querySelector(".mode");
 let body = document.getElementById("body");
 let head = document.querySelector(".head");
+let fullTodo = document.querySelector(".to-do")
 let currentTheme = "white";
 let width = window.innerWidth
 
@@ -39,10 +40,10 @@ let setBgs = (mode, id, width) => {
 
 // Set the Mode Todo colour
 let setTodos = (mode, id) => {
-    let toDoContainer = document.querySelectorAll(".to-do-cont")
-    let toDoBox = document.querySelectorAll(".to-do-box")
-    let circles = document.querySelectorAll(".circle")
-    let toDoText = document.querySelectorAll(".to-do-input");
+    let toDoContainer = fullTodo.querySelectorAll(".to-do-cont")
+    let toDoBox = fullTodo.querySelectorAll(".to-do-box")
+    let circles = fullTodo.querySelectorAll(".circle")
+    let toDoText = fullTodo.querySelectorAll(".to-do-input");
 
     toDoContainer.forEach((cont) => {
         cont.style.backgroundColor = mode[id].toDoContainer
@@ -76,11 +77,22 @@ themeIcon.addEventListener("click", (e) => {
     }
 })
 
+// function to check mode
+let checkMode = () => {
+    if(currentTheme == "black"){
+        setTodos(mode, 0);
+    }else if(currentTheme == "white"){
+        setTodos(mode, 1);
+    }
+}
+
 
 // Form section
 let form = document.querySelector("form");
 let toDos = document.querySelector("ul");
 let clearFilter = document.querySelector(".clear-filter")
+let allTodos = document.querySelector("#to-do-remain");
+let clearAllTodos = document.querySelector("#to-do-clear");
 
 // Template to create a new todo
 let createTemplate = (todo) => {
@@ -92,20 +104,51 @@ let createTemplate = (todo) => {
     toDos.innerHTML += html;
 }
 
+// Template to show empty filtered todo
+let noTodo = (filter) => {
+    let html = `<li id="emptyTodo" class="to-do-box to-do-cont no-toDo to-do-checked">
+                    <p class="no-to-do">${filter}</p>
+                </li>`
+    toDos.innerHTML += html;
+}
+
+// Function that removes nodes with No Todo
+let removeNoTodo = () => {
+    Array.from(toDos.children)
+        .forEach(todo => {
+            if(todo.classList.contains("no-toDo")){
+                todo.remove();
+            }
+        })
+}
+// Function that handles current amount of todos
+function todoCount(filter){
+    let items = Array.from(toDos.children)
+                    .filter(todo => !todo.classList.contains("to-do-filtered"))
+    if(items.length == 0){
+        if(filter == "active" || filter == "completed"){
+            noTodo(`No Todo is ${filter} at the moment`)
+            checkMode();
+            allTodos.innerText = items.length;
+        }else{
+            clearFilter.style.display = "none";
+        }
+    }else{
+        allTodos.innerText = items.length;
+    }
+}
+
 // handling the form
 form.addEventListener("submit", e => {
     e.preventDefault();
     let todo = form.toDoInput.value.trim();
     if (todo.length){
         createTemplate(todo);
+        checkMode();
         form.reset();
-    }
+    }    
     clearFilter.style.display = "grid";
-    if(currentTheme == "black"){
-        setTodos(mode, 0);
-    }else if(currentTheme == "white"){
-        setTodos(mode, 1);
-    }
+    todoCount();
 })
 
 // delete a todo
@@ -113,6 +156,7 @@ toDos.addEventListener("click", e => {
     if (e.target.className == "delete"){
         e.target.parentElement.remove();
     }
+    todoCount();
 })
 
 // Handles marking of todo as completed
@@ -122,4 +166,52 @@ toDos.addEventListener("click", e => {
     }else if(e.target.parentElement.classList.contains("to-do-circle")){
         e.target.parentElement.parentElement.classList.toggle("to-do-checked")
     }
+})
+
+// Handles clearing of completed todos
+clearAllTodos.addEventListener("click", () => {
+    if (confirm("Are you sure you want to clear all completed todos") == true){
+        let childArray = Array.from(toDos.children);
+        childArray.forEach(child => {
+            if(child.classList.contains("to-do-checked")){child.remove()}
+        })
+        todoCount("completed");
+        form.reset();
+    }
+})
+
+
+// Filtering Todos
+
+// function that filters todo
+let filterTodo = (id) => {
+    if (id == "active"){
+        Array.from(toDos.children)
+            .filter((todo) => todo.classList.contains("to-do-checked"))
+            .forEach(todo => todo.classList.add("to-do-filtered"))
+        Array.from(toDos.children)
+            .filter((todo) => !todo.classList.contains("to-do-checked"))
+            .forEach(todo => todo.classList.remove("to-do-filtered"))
+    }
+    if (id == "completed"){
+        Array.from(toDos.children)
+            .filter((todo) => !todo.classList.contains("to-do-checked"))
+            .forEach(todo => todo.classList.add("to-do-filtered"))
+         Array.from(toDos.children)
+            .filter((todo) => todo.classList.contains("to-do-checked"))
+            .forEach(todo => todo.classList.remove("to-do-filtered"))
+    }
+    if (id == "all"){
+        Array.from(toDos.children)
+            .filter((todo) => todo.classList.contains("add-to-do"))
+            .forEach(todo => todo.classList.remove("to-do-filtered"))
+    }
+}
+
+// Event listener to filter todo
+let filter = document.querySelector(".to-do-filter");
+filter.addEventListener("click", e => {
+    filterTodo(e.target.id);
+    removeNoTodo()
+    todoCount(e.target.id);
 })
